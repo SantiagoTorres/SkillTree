dff6545a861ee80f499e6329cb40a7e044e9ee33
Miguel Kelly <m26.kelly@gmail.com>
2013-04-23 07:39:31 -0500

    Si ya dejame de chingar

--- src/JFrameTester.java
+++ src/JFrameTester.java
@@ -4,11 +4,14 @@ public class JFrameTester {
 	
 
 	public static void main(String[] args) {
-		TreeView treeView;
-		JFrame f = new JFrame("A JFrame");
-		treeView =  new TreeView(30,30,f.getContentPane());
-		GridBagConstraints c = new GridBagConstraints();
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    //TreeView treeView;
+	    SkillToolTip skillToolTip;
+	    JFrame f = new JFrame("A JFrame");
+		/*treeView =  new TreeView(30,30,f.getContentPane());*/
+	    //	GridBagConstraints c = new GridBagConstraints();
+		/*skillToolTip = new SkillToolTip(new SkillModel(),f.getContentPane());*/
+		SkillDetailView Sdk = new SkillDetailView(new SkillModel());
+			f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setSize(250, 250);
 		f.setLocation(300,200);
 		f.pack();
