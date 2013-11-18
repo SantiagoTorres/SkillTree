af2b3da8f670e3992935ac88d6102909b5866b7c
Santiago Torres <torresariass@gmail.com>
2013-11-18 00:32:51 -0500

    Modified the startup routine, no empty skill is added now

--- src/SkillTree.java
+++ src/SkillTree.java
@@ -11,13 +11,12 @@ public class SkillTree {
 		TreeView treeView;
 		MainWindow mainWindow;
 		CreateSkill theSkill;
-		TreeModel treeModel = new TreeModel(new SkillModel());
+		TreeModel treeModel = new TreeModel();
 		JLabel theLabel = new JLabel("lulz");
 		SkillView skillView = new SkillView(new String("name"),new String("0/1"),"../lib/resources/images/wolf.png");
 		treeView =  new TreeView(2,2);
 		treeView.addComponentAt(skillView,0,0);
 		mainWindow = new MainWindow(treeView,treeModel);
-		theSkill = new CreateSkill(treeModel);
 	}
 	
 }
